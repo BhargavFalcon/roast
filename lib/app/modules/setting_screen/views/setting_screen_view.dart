d9c5b48707086a766af3f45c8de56d31bed7c8d2
Khilan Vitthani <bhargav@falconsolutions.co>
2025-08-26 14:56:43 +0530

    roast screen button issue fix done

--- lib/app/modules/setting_screen/views/setting_screen_view.dart
+++ lib/app/modules/setting_screen/views/setting_screen_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:roast/app/constants/sizeConstant.dart';
 
 import '../controllers/setting_screen_controller.dart';
 
@@ -9,14 +9,25 @@ class SettingScreenView extends GetView<SettingScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SettingScreenView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SettingScreenView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 5),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: MySize.getHeight(24),
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: MySize.getHeight(10)),
+            Expanded(child: Column(children: [])),
+          ],
         ),
       ),
     );
