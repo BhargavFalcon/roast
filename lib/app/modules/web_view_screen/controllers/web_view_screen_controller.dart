a43b066bdd830ff27f4743917bc184f407fbb75e
Khilan Vitthani <bhargav@falconsolutions.co>
2025-08-29 16:48:15 +0530

    webview done

---
+++ lib/app/modules/web_view_screen/controllers/web_view_screen_controller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:roast/app/constants/api_constants.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreenController extends GetxController {
+  RxBool isWebViewLoading = true.obs;
+  RxString title = "".obs;
+  WebViewController? webViewController;
+  RxString webUrl = "".obs;
+  @override
+  void onInit() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if (Get.arguments != null) {
+        webUrl.value = Get.arguments[ArgumentConstant.url];
+      }
+      webViewController =
+          WebViewController()
+            ..setJavaScriptMode(JavaScriptMode.unrestricted)
+            ..setNavigationDelegate(
+              NavigationDelegate(
+                onProgress: (int progress) {
+                  if (progress == 100) {
+                    isWebViewLoading.value = false;
+                  }
+                },
+                onPageStarted: (String url) {},
+                onPageFinished: (String url) {},
+                onHttpError: (HttpResponseError error) {},
+                onWebResourceError: (WebResourceError error) {},
+              ),
+            )
+            ..loadRequest(Uri.parse("$webUrl"));
+    });
+
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
---
+++ lib/app/modules/web_view_screen/views/web_view_screen_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart' show CupertinoActivityIndicator;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:roast/app/constants/color_constant.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../controllers/web_view_screen_controller.dart';
+
+class WebViewScreenView extends GetWidget<WebViewScreenController> {
+  const WebViewScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<WebViewScreenController>(
+      init: WebViewScreenController(),
+      assignId: true,
+      builder: (controller) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              controller.webUrl.value,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+              ),
+            ),
+            centerTitle: true,
+          ),
+          body:
+              (controller.isWebViewLoading.isTrue)
+                  ? Center(
+                    child: CupertinoActivityIndicator(
+                      color: ColorConstants.primaryColor,
+                    ),
+                  )
+                  : WebViewWidget(controller: controller.webViewController!),
+        );
+      },
+    );
+  }
+}
